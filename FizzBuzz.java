83bd224664f3d7ab72fc07ba0de102ab46d55c87
Mikhail Bhuta <mikhail.b1990@gmail.com>
2013-10-30 11:15:21 -0700

    Added fizz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -4,7 +4,8 @@ public class FizzBuzz
 {
 	pubnlic static void main(String[] args) {
 		for (int i = 0; i < 100; i++) {
-			System.out.println(i);
+			if( i % 3 == 0)
+				System.out.println(i + ": " + "Fizz");
 		}
 	}
 }
